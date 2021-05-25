bdc02ed999df661cd6c30349ed295df860e0911b
Guna7808 <gunasekharpallam454@gmail.com>
2021-05-25 15:10:26 +0530

    pojo classes

---
+++ src/main/java/com/test/APIAutomation/OneDat/Pojo/ApiCourses.java
@@ -0,0 +1,21 @@
+package com.test.APIAutomation.OneDat.Pojo;
+
+public class ApiCourses {
+	
+	private String courseTitle;
+	private String price;
+	
+	public String getCourseTitle() {
+		return courseTitle;
+	}
+	public void setCourseTitle(String courseTitle) {
+		this.courseTitle = courseTitle;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}

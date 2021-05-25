ce2d0b0c4a99e60f4522ccd1985d15d8b9f77662
Guna7808 <gunasekharpallam454@gmail.com>
2021-05-25 19:03:08 +0530

    pojo classes

--- src/main/java/com/test/APIAutomation/OneDat/Pojo/MobileCourses.java
+++ src/main/java/com/test/APIAutomation/OneDat/Pojo/MobileCourses.java
@@ -1,5 +1,21 @@
 package com.test.APIAutomation.OneDat.Pojo;
 
 public class MobileCourses {
+	
+
+	private String courseTitle;
+	private String price;
+	public String getCourseTitle() {
+		return courseTitle;
+	}
+	public void setCourseTitle(String courseTitle) {
+		this.courseTitle = courseTitle;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
 
 }
--- src/main/java/com/test/APIAutomation/OneDat/Pojo/WebAutomationCourses.java
+++ src/main/java/com/test/APIAutomation/OneDat/Pojo/WebAutomationCourses.java
@@ -1,5 +1,20 @@
 package com.test.APIAutomation.OneDat.Pojo;
 
 public class WebAutomationCourses {
+	
+	private String courseTitle;
+	public String getCourseTitle() {
+		return courseTitle;
+	}
+	public void setCourseTitle(String courseTitle) {
+		this.courseTitle = courseTitle;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	private String price;
 
 }
